a89bf48b4054afcb12858ee2f2bbcb4795053bc4
monologg <adieujw@gmail.com>
2022-01-20 18:54:01 +0000

    docs: update readme

--- README.md
+++ README.md
@@ -1,3 +1,51 @@
 # EncT5
 
-Pytorch Implementation of [EncT5: Fine-tuning T5 Encoder for Non-autoregressive Tasks](https://arxiv.org/abs/2110.08426)
+(Unofficial) Pytorch Implementation of `EncT5`: [Fine-tuning T5 Encoder for Non-autoregressive Tasks](https://arxiv.org/abs/2110.08426)
+
+## About
+
+<div align="center">
+    <img src="https://user-images.githubusercontent.com/28896432/150393562-a0b3af15-a1f5-43a4-af6f-0954fafb2458.png" width="600px">
+</div>
+
+- Finetune T5 model for classification & regression by **only using the encoder layers**.
+- Implemented of [`Tokenizer`](./enc_t5/tokenization_enc_t5.py) and [`Model`](./enc_t5/modeling_enc_t5.py) for EncT5.
+- Add **BOS Token (`<s>`)** for tokenizer, and use this token for classification & regression.
+  - Need to resize embedding as vocab size is changed. (`model.resize_token_embeddings()`)
+
+## Requirements
+
+> Highly recommend to use the same version of `transformers`.
+
+```
+transformers==4.15.0
+torch==1.8.1
+sentencepiece==0.1.96
+datasets==1.17.0
+scikit-learn==0.24.2
+```
+
+## Finetune on GLUE
+
+- Use `Huggingface Transformers Trainer` for finetuning GLUE Task.
+- See more details in [Trainer Guideline](https://github.com/huggingface/transformers/blob/e03544a13804a32ff12afff98c8e60faa0fdc282/examples/pytorch/README.md) and [Trainer Documentation](https://huggingface.co/docs/transformers/v4.15.0/en/main_classes/trainer).
+
+### Setup
+
+- Use [`T5 1.1 base`](https://huggingface.co/google/t5-v1_1-base) for finetuning.
+- Evaluate on TPU. See [`run_glue_tpu.sh`](./scripts/run_glue_tpu.sh) for more details.
+- Use **`AdamW`** optimizer instead of `Adafactor`.
+- Check best checkpoint on every epoch by using `EarlyStoppingCallback`.
+
+### Results
+
+| Task  | Metric      | Result (Paper) | Result (Implementation) |
+| :---- | ----------- | :------------: | :---------------------: |
+| CoLA  | Matthew     |      53.1      |                         |
+| SST-2 | Acc         |      94.0      |                         |
+| MRPC  | F1/Acc      |   91.5/88.3    |                         |
+| STS-B | PCC/SCC     |   80.5/79.3    |                         |
+| QQP   | F1/Acc      |   72.9/89.8    |                         |
+| MNLI  | Mis/Matched |   88.0/86.7    |                         |
+| QNLI  | Acc         |      93.3      |                         |
+| RTE   | Acc         |      67.8      |                         |
---
+++ run_glue_tpu.sh
@@ -0,0 +1,224 @@
+#!/bin/bash
+export XRT_TPU_CONFIG="localservice;0;localhost:51011"
+
+MODEL_NAME=google/t5-v1_1-base
+
+TASK_NAME=cola
+
+python3 xla_spawn.py --num_cores 8 \
+    run_glue.py \
+    --model_name_or_path $MODEL_NAME \
+    --task_name $TASK_NAME \
+    --do_train \
+    --do_eval \
+    --max_seq_length 512 \
+    --per_device_train_batch_size 16 \
+    --per_device_eval_batch_size 32 \
+    --learning_rate 5e-5 \
+    --adafactor \
+    --num_train_epochs 20 \
+    --logging_steps 100 \
+    --output_dir eval_outputs/$TASK_NAME \
+    --overwrite_output_dir \
+    --cache_dir cache \
+    --seed 42 \
+    --evaluation_strategy epoch \
+    --save_strategy epoch \
+    --load_best_model_at_end True \
+    --metric_for_best_model eval_matthews_correlation \
+    --greater_is_better True \
+    --early_stopping_patience 5
+    
+TASK_NAME=sst2
+
+python3 xla_spawn.py --num_cores 8 \
+    run_glue.py \
+    --model_name_or_path $MODEL_NAME \
+    --task_name $TASK_NAME \
+    --do_train \
+    --do_eval \
+    --max_seq_length 512 \
+    --per_device_train_batch_size 16 \
+    --per_device_eval_batch_size 32 \
+    --learning_rate 5e-5 \
+    --warmup_ratio 0.1 \
+    --num_train_epochs 20 \
+    --logging_steps 100 \
+    --output_dir eval_outputs/$TASK_NAME \
+    --overwrite_output_dir \
+    --cache_dir cache \
+    --seed 42 \
+    --evaluation_strategy epoch \
+    --save_strategy epoch \
+    --load_best_model_at_end True \
+    --metric_for_best_model eval_accuracy \
+    --greater_is_better True \
+    --early_stopping_patience 5
+
+TASK_NAME=mrpc
+
+python3 xla_spawn.py --num_cores 8 \
+    run_glue.py \
+    --model_name_or_path $MODEL_NAME \
+    --task_name $TASK_NAME \
+    --do_train \
+    --do_eval \
+    --max_seq_length 512 \
+    --per_device_train_batch_size 16 \
+    --per_device_eval_batch_size 32 \
+    --learning_rate 5e-5 \
+    --warmup_ratio 0.1 \
+    --num_train_epochs 20 \
+    --logging_steps 100 \
+    --output_dir eval_outputs/$TASK_NAME \
+    --overwrite_output_dir \
+    --cache_dir cache \
+    --seed 42 \
+    --evaluation_strategy epoch \
+    --save_strategy epoch \
+    --load_best_model_at_end True \
+    --metric_for_best_model eval_f1 \
+    --greater_is_better True \
+    --early_stopping_patience 5
+
+rm -rf eval_outputs/*/checkpoint*
+
+TASK_NAME=stsb
+
+python3 xla_spawn.py --num_cores 8 \
+    run_glue.py \
+    --model_name_or_path $MODEL_NAME \
+    --task_name $TASK_NAME \
+    --do_train \
+    --do_eval \
+    --max_seq_length 512 \
+    --per_device_train_batch_size 16 \
+    --per_device_eval_batch_size 32 \
+    --learning_rate 5e-5 \
+    --warmup_ratio 0.1 \
+    --num_train_epochs 20 \
+    --logging_steps 100 \
+    --output_dir eval_outputs/$TASK_NAME \
+    --overwrite_output_dir \
+    --cache_dir cache \
+    --seed 42 \
+    --evaluation_strategy epoch \
+    --save_strategy epoch \
+    --load_best_model_at_end True \
+    --metric_for_best_model eval_combined_score \
+    --greater_is_better True \
+    --early_stopping_patience 5
+
+rm -rf eval_outputs/*/checkpoint*
+
+TASK_NAME=qqp
+
+python3 xla_spawn.py --num_cores 8 \
+    run_glue.py \
+    --model_name_or_path $MODEL_NAME \
+    --task_name $TASK_NAME \
+    --do_train \
+    --do_eval \
+    --max_seq_length 512 \
+    --per_device_train_batch_size 16 \
+    --per_device_eval_batch_size 32 \
+    --learning_rate 5e-5 \
+    --warmup_ratio 0.1 \
+    --num_train_epochs 20 \
+    --logging_steps 100 \
+    --output_dir eval_outputs/$TASK_NAME \
+    --overwrite_output_dir \
+    --cache_dir cache \
+    --seed 42 \
+    --evaluation_strategy epoch \
+    --save_strategy epoch \
+    --load_best_model_at_end True \
+    --metric_for_best_model eval_accuracy \
+    --greater_is_better True \
+    --early_stopping_patience 5
+
+rm -rf eval_outputs/*/checkpoint*
+
+TASK_NAME=mnli
+
+python3 xla_spawn.py --num_cores 8 \
+    run_glue.py \
+    --model_name_or_path $MODEL_NAME \
+    --task_name $TASK_NAME \
+    --do_train \
+    --do_eval \
+    --max_seq_length 512 \
+    --per_device_train_batch_size 16 \
+    --per_device_eval_batch_size 32 \
+    --learning_rate 5e-5 \
+    --warmup_ratio 0.1 \
+    --num_train_epochs 20 \
+    --logging_steps 100 \
+    --output_dir eval_outputs/$TASK_NAME \
+    --overwrite_output_dir \
+    --cache_dir cache \
+    --seed 42 \
+    --evaluation_strategy epoch \
+    --save_strategy epoch \
+    --load_best_model_at_end True \
+    --metric_for_best_model eval_accuracy \
+    --greater_is_better True \
+    --early_stopping_patience 5
+
+rm -rf eval_outputs/*/checkpoint*
+
+TASK_NAME=qnli
+
+python3 xla_spawn.py --num_cores 8 \
+    run_glue.py \
+    --model_name_or_path $MODEL_NAME \
+    --task_name $TASK_NAME \
+    --do_train \
+    --do_eval \
+    --max_seq_length 512 \
+    --per_device_train_batch_size 16 \
+    --per_device_eval_batch_size 32 \
+    --learning_rate 5e-5 \
+    --warmup_ratio 0.1 \
+    --num_train_epochs 20 \
+    --logging_steps 100 \
+    --output_dir eval_outputs/$TASK_NAME \
+    --overwrite_output_dir \
+    --cache_dir cache \
+    --seed 42 \
+    --evaluation_strategy epoch \
+    --save_strategy epoch \
+    --load_best_model_at_end True \
+    --metric_for_best_model eval_accuracy \
+    --greater_is_better True \
+    --early_stopping_patience 5
+
+rm -rf eval_outputs/*/checkpoint*
+
+TASK_NAME=rte
+
+python3 xla_spawn.py --num_cores 8 \
+    run_glue.py \
+    --model_name_or_path $MODEL_NAME \
+    --task_name $TASK_NAME \
+    --do_train \
+    --do_eval \
+    --max_seq_length 512 \
+    --per_device_train_batch_size 16 \
+    --per_device_eval_batch_size 32 \
+    --learning_rate 5e-5 \
+    --warmup_ratio 0.1 \
+    --num_train_epochs 20 \
+    --logging_steps 100 \
+    --output_dir eval_outputs/$TASK_NAME \
+    --overwrite_output_dir \
+    --cache_dir cache \
+    --seed 42 \
+    --evaluation_strategy epoch \
+    --save_strategy epoch \
+    --load_best_model_at_end True \
+    --metric_for_best_model eval_accuracy \
+    --greater_is_better True \
+    --early_stopping_patience 5
+
+rm -rf eval_outputs/*/checkpoint*
